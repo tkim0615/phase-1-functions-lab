a6388b7667a9c9e3bf79a5bf0c80f56c4e3bd06f
tyler kim <yoonsoo.kim11@gmail.com>
2023-11-13 19:24:42 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue){
+    return Math.abs(someValue - 42)
+}
+
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue) * 264
+    
+}
+
+function distanceTravelledInFeet(start, destination) {
+return Math.abs(start - destination) * 264
+}
+
+function calculatesFarePrice(start, destination){
+    let distanceInFeet = distanceTravelledInFeet(start, destination)
+    if(distanceInFeet <= 400) {
+        return 0
+    } else
+    if( distanceInFeet > 400 && distanceInFeet < 2000){
+        return (distanceInFeet - 400) * 0.02
+    } else
+    if(distanceInFeet > 2000 && distanceInFeet < 2500) {
+        return 25
+    }
+    else{
+       return 'cannot travel that far'}
+}
